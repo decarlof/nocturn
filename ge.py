5de13d175294e9f84c1b1743816d5a2841f017e7
Francesco De Carlo <decarlof@gmail.com>
2024-06-28 15:38:31 +0200

    added support for multiple metadata folder

--- ge.py
+++ ge.py
@@ -103,86 +103,87 @@ def main(args):
                 file_name_pcr   = p.with_suffix('.pcr')
                 file_name_dtxml = p.with_suffix('.dtxml')
                 file_name_xlsx  = p.parents[1].joinpath('master').with_suffix('.xlsx')
+
+                # print('1', p)
+                # print('2', p.suffix)
+                # print('3', p.parents[0])
+                # print('3', p.parents[1])
+                # print('4', p.stem)
+                # print('5', p.suffix)
+
+                # print(file_name_pca  ) 
+                # print(file_name_pcp  ) 
+                # print(file_name_pcr  ) 
+                # print(file_name_dtxml) 
+                # print(file_name_xlsx ) 
+
+                my_dict = {}
+
+                scan_time, datetime_start = extract_meta_from_pcp(file_name_pcp)
+                my_pcr_dict = extract_meta_from_config(file_name_pcr)
+                my_pca_dict = extract_meta_from_config(file_name_pca)
+                my_xml_dict = extract_meta_from_dtxml(file_name_dtxml)
+
+                # Dictionary keys assignment. The order of the assigment will sort the xlsx file columns 
+                my_dict['scan date']                           = datetime_start
+                my_dict['Operator']                            = my_xml_dict['Operator']
+                my_dict['Researcher']                          = my_xml_dict['Researcher']
+                my_dict['NMNH PI']                             = my_xml_dict['NMNH PI']
+                my_dict['Department']                          = my_xml_dict['Department']
+                my_dict['CT project #']                        = my_xml_dict['Project Number']
+                my_dict['specimen ID or USNM#']                = my_xml_dict['Sample ID']
+                my_dict['Species name']                        = my_xml_dict['Sample Name']
+                my_dict['stain']                               = my_xml_dict['Description']
+                try: 
+                    my_dict['Sample type']                      = my_xml_dict['Sample Type']
+                except KeyError: 
+                    print('Sample type is missing, added empty field')
+                    my_dict['Sample type']                      = 'Empty'
+                my_dict['folder name']                         = p.stem
+                # my_dict['Folder']                              = my_pcr_dict['ImageData']['pca_file']
+                my_dict['timing (ms)']                         = my_pca_dict['Detector']['timingval']
+                my_dict['frame avg']                           = my_pca_dict['Detector']['avg']
+                my_dict['skip']                                = my_pca_dict['CT']['skipacc']
+                my_dict['binning']                             = my_pca_dict['Detector']['binning']
+                my_dict['sensitivity']                         = my_pca_dict['Detector']['cameragain']
+                my_dict['# images']                            = my_pca_dict['CT']['numberimages']
+                my_dict['total scanning time (hrs)']           = str(scan_time)
+                my_dict['voltage (kV)']                        = my_pca_dict['Xray']['voltage']
+                my_dict['current (uA)']                        = my_pca_dict['Xray']['current']
+                my_dict['act. power (W)']                      = "Manual Entry"
+                my_dict['magnification']                       = my_pca_dict['Geometry']['magnification']
+                my_dict['voxel size (um)']                     = float(my_pca_dict['Geometry']['voxelsizex']) * 1000.0
+                my_dict['tube type']                           = my_pca_dict['Xray']['name']
+                my_dict['mode']                                = my_pca_dict['Xray']['mode']
+                my_dict['multiscan (# of scans)']              = my_pca_dict['Multiscan']['active']
+                my_dict['filter']                              = "Manual Entry"
+                my_dict['target']                              = "Manual Entry"
+                my_dict['collimator']                          = my_pca_dict['Xray']['collimation']
+
+                # Specific to Freya
+                my_dict['Trawl number or collection event ID'] = "Manual Entry"
+                my_dict['same specimen as Scan x']             = "Manual Entry"
+                my_dict['specimen pixel width']                = "Manual Entry"
+
+                # additional meta data
+                my_dict['general system name']                 = my_pca_dict['General']['systemname']
+
+                df = pd.DataFrame(data={**my_dict}, index=[0])
+                xlsx = pathlib.Path(file_name_xlsx)
+                if xlsx.is_file():
+                    wb = load_workbook(filename = file_name_xlsx)
+                    ws = wb["Sheet1"]
+                    for r in dataframe_to_rows(df, index=False, header=False):  #No index and don't append the column headers
+                        ws.append(r)
+                    wb.save(file_name_xlsx)
+                    # df.to_excel(file_name_xlsx, header=False, index=False)
+                    print('Append to existing meta-data excel file at: %s' % file_name_xlsx)
+                else:
+                    df.to_excel(file_name_xlsx, header=True, index=False)
+                    print('Create a new meta-data excel file at: %s' % file_name_xlsx)
+
             else:
                 print('ERROR: %s does not exist' % p)
-                sys.exit(1)
-    # print('1', p)
-    # print('2', p.suffix)
-    # print('3', p.parents[0])
-    # print('3', p.parents[1])
-    # print('4', p.stem)
-    # print('5', p.suffix)
-
-    # print(file_name_pca  ) 
-    # print(file_name_pcp  ) 
-    # print(file_name_pcr  ) 
-    # print(file_name_dtxml) 
-    # print(file_name_xlsx ) 
-
-    my_dict = {}
-
-    scan_time, datetime_start = extract_meta_from_pcp(file_name_pcp)
-    my_pcr_dict = extract_meta_from_config(file_name_pcr)
-    my_pca_dict = extract_meta_from_config(file_name_pca)
-    my_xml_dict = extract_meta_from_dtxml(file_name_dtxml)
-
-    # Dictionary keys assignment. The order of the assigment will sort the xlsx file columns 
-    my_dict['scan date']                           = datetime_start
-    my_dict['Operator']                            = my_xml_dict['Operator']
-    my_dict['Researcher']                          = my_xml_dict['Researcher']
-    my_dict['NMNH PI']                             = my_xml_dict['NMNH PI']
-    my_dict['Department']                          = my_xml_dict['Department']
-    my_dict['CT project #']                        = my_xml_dict['Project Number']
-    my_dict['specimen ID or USNM#']                = my_xml_dict['Sample ID']
-    my_dict['Species name']                        = my_xml_dict['Sample Name']
-    my_dict['stain']                               = my_xml_dict['Description']
-    try: 
-        my_dict['Sample type']                      = my_xml_dict['Sample Type']
-    except KeyError: 
-        print('Sample type is missing, added empty field')
-        my_dict['Sample type']                      = 'Empty'
-    my_dict['folder name']                         = p.stem
-    # my_dict['Folder']                              = my_pcr_dict['ImageData']['pca_file']
-    my_dict['timing (ms)']                         = my_pca_dict['Detector']['timingval']
-    my_dict['frame avg']                           = my_pca_dict['Detector']['avg']
-    my_dict['skip']                                = my_pca_dict['CT']['skipacc']
-    my_dict['binning']                             = my_pca_dict['Detector']['binning']
-    my_dict['sensitivity']                         = my_pca_dict['Detector']['cameragain']
-    my_dict['# images']                            = my_pca_dict['CT']['numberimages']
-    my_dict['total scanning time (hrs)']           = str(scan_time)
-    my_dict['voltage (kV)']                        = my_pca_dict['Xray']['voltage']
-    my_dict['current (uA)']                        = my_pca_dict['Xray']['current']
-    my_dict['act. power (W)']                      = "Manual Entry"
-    my_dict['magnification']                       = my_pca_dict['Geometry']['magnification']
-    my_dict['voxel size (um)']                     = float(my_pca_dict['Geometry']['voxelsizex']) * 1000.0
-    my_dict['tube type']                           = my_pca_dict['Xray']['name']
-    my_dict['mode']                                = my_pca_dict['Xray']['mode']
-    my_dict['multiscan (# of scans)']              = my_pca_dict['Multiscan']['active']
-    my_dict['filter']                              = "Manual Entry"
-    my_dict['target']                              = "Manual Entry"
-    my_dict['collimator']                          = my_pca_dict['Xray']['collimation']
-
-    # Specific to Freya
-    my_dict['Trawl number or collection event ID'] = "Manual Entry"
-    my_dict['same specimen as Scan x']             = "Manual Entry"
-    my_dict['specimen pixel width']                = "Manual Entry"
-
-    # additional meta data
-    my_dict['general system name']                 = my_pca_dict['General']['systemname']
-
-    df = pd.DataFrame(data={**my_dict}, index=[0])
-    xlsx = pathlib.Path(file_name_xlsx)
-    if xlsx.is_file():
-        wb = load_workbook(filename = file_name_xlsx)
-        ws = wb["Sheet1"]
-        for r in dataframe_to_rows(df, index=False, header=False):  #No index and don't append the column headers
-            ws.append(r)
-        wb.save(file_name_xlsx)
-        # df.to_excel(file_name_xlsx, header=False, index=False)
-        print('Append to existing meta-data excel file at: %s' % file_name_xlsx)
-    else:
-        df.to_excel(file_name_xlsx, header=True, index=False)
-        print('Create a new meta-data excel file at: %s' % file_name_xlsx)
 
 if __name__ == "__main__":
    main(sys.argv)
